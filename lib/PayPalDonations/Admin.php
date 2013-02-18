084d6b13e85793195398b0a32c8de73565d4e725
Johan Steen <artstorm@gmail.com>
2013-02-18 23:23:03 +0700

    Converts tabs to spaces to comply with PSR-2.

--- lib/PayPalDonations/Admin.php
+++ lib/PayPalDonations/Admin.php
@@ -11,23 +11,23 @@
  */
 class PayPalDonations_Admin
 {
-	private $plugin_options;
-	private $currency_codes;
-	private $donate_buttons;
-	private $localized_buttons;
-	private $checkout_languages;
+    private $plugin_options;
+    private $currency_codes;
+    private $donate_buttons;
+    private $localized_buttons;
+    private $checkout_languages;
 
-	public function setOptions( $options, $code, $buttons, $loc_buttons, $checkout_lng )
-	{
-		$this->plugin_options = $options;
-		$this->currency_codes = $code;
-		$this->donate_buttons = $buttons;
-		$this->localized_buttons = $loc_buttons;
-		$this->checkout_languages = $checkout_lng;
-	}
+    public function setOptions( $options, $code, $buttons, $loc_buttons, $checkout_lng )
+    {
+        $this->plugin_options = $options;
+        $this->currency_codes = $code;
+        $this->donate_buttons = $buttons;
+        $this->localized_buttons = $loc_buttons;
+        $this->checkout_languages = $checkout_lng;
+    }
 
-	public function render()
-	{
+    public function render()
+    {
         $data = array(
             'plugin_options' => $this->plugin_options,
             'currency_codes' => $this->currency_codes,
@@ -36,27 +36,27 @@ class PayPalDonations_Admin
             'checkout_languages' => $this->checkout_languages,
         );
         echo PayPalDonations_View::render(plugin_dir_path(__FILE__).'../../views/admin.php', $data);
-	}
+    }
 
-	// -------------------------------------------------------------------------
-	// HTML and Form element methods
-	// -------------------------------------------------------------------------
-	
-	/**
-	 * Checkbox.
-	 * Renders the HTML for an input checkbox.
-	 *
-	 * @param	string	$label		The label rendered to screen
-	 * @param	string	$name		The unique name to identify the input
-	 * @param	boolean	$checked	If the input is checked or not
-	 */
-	static function checkbox( $label, $name, $checked )
-	{
-		printf( '<input type="checkbox" name="%s" value="true"', $name );
-		if ($checked)
-			echo ' checked';
-		echo ' />';
-		echo ' '.$label;
-	}
+    // -------------------------------------------------------------------------
+    // HTML and Form element methods
+    // -------------------------------------------------------------------------
+    
+    /**
+     * Checkbox.
+     * Renders the HTML for an input checkbox.
+     *
+     * @param   string  $label      The label rendered to screen
+     * @param   string  $name       The unique name to identify the input
+     * @param   boolean $checked    If the input is checked or not
+     */
+    static function checkbox( $label, $name, $checked )
+    {
+        printf( '<input type="checkbox" name="%s" value="true"', $name );
+        if ($checked)
+            echo ' checked';
+        echo ' />';
+        echo ' '.$label;
+    }
 
 }
