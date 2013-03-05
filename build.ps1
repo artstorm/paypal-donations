b7404d32b633924aac1a15f22db109b30ad30da9
Johan Steen <artstorm@gmail.com>
2013-03-05 16:29:39 +0700

    Adds bump method. Fixes #23.

--- build.ps1
+++ build.ps1
@@ -55,36 +55,75 @@ function build_plugin
     Write-Host "Plugin successfully built! - $DATE"
 }
 
+function bump($newVersion)
+{
+    $oldVersion = findVersionNumber
+
+    Write-Host $('-' * 80)
+    Write-Host "BUMP" -foregroundcolor "White"
+    Write-Host "Bumping $oldVersion to $newVersion" -noNewLine
+    # Let's have some dots printed out. Makes it old skool
+    for ($ctr = 0; $ctr -lt 3; $ctr++) {
+        Write-Host "." -noNewLine
+        sleep(1)
+    }
+
+
+    cat $PLUGIN_FILE `
+        | %{$_ -replace "Version: $oldVersion", "Version: $newVersion"} `
+        | Set-Content "$($PLUGIN_FILE).tmp"
+
+    correctEncoding("$($PLUGIN_FILE).tmp")
+
+    # Copy and clean up
+    cp "$($PLUGIN_FILE).tmp" $PLUGIN_FILE
+    Remove-Item "$($PLUGIN_FILE).tmp"
+
+    Write-Host "Done!"
+    Write-Host $('-' * 80)
+}
+
+function correctEncoding($file)
+{
+    # Set UNIX line endings and UTF-8 encoding.
+    Get-ChildItem $file | ForEach-Object {
+        # get the contents and replace line breaks by U+000A
+        $contents = [IO.File]::ReadAllText($_) -replace "`r`n?", "`n"
+        # create UTF-8 encoding without signature
+        $utf8 = New-Object System.Text.UTF8Encoding $false
+        # write the text back
+        [IO.File]::WriteAllText($_, $contents, $utf8)
+    }
+}
+
 function findVersionNumber
 {
-  # The file comes in as an array (one line per key)
-  $plugin = cat $PLUGIN_FILE
-  # Convert it to string, with new lines added
-  $plugin = [string]::Join("`n", ($plugin))
+    # The file comes in as an array (one line per key)
+    $plugin = cat $PLUGIN_FILE
+    # Convert it to string, with new lines added
+    $plugin = [string]::Join("`n", ($plugin))
 
-  # Search the plugin for the current version number
-  $regex = [regex]"(?<=Version:)[^`n]*"
-  $version =  $regex.Match($plugin).Value
+    # Search the plugin for the current version number
+    $regex = [regex]"(?<=Version:)[^`n]*"
+    $version =  $regex.Match($plugin).Value
 
-  # Trim away white space, and convert from string to decimal
-  $version = [decimal] $version.trim()
+    # Trim away white space, and convert from string to decimal
+    $version = [decimal] $version.trim()
 
-  return $version
+    return $version
 }
 
 function header
 {
     Write-Host $('-' * 80)
-    Write-Host 'PayPal Donations'
-    Write-Host 'Version: ' -NoNewLine
-    findVersionNumber
+    Write-Host 'PAYPAL DONATIONS' -foregroundcolor "White"
+    Write-Host "Version: $(findVersionNumber)"
     Write-Host $('-' * 80)
 }
 
 function checklist
 {
     Write-Host "Checklist"  -foregroundcolor "Red"
-    Write-Host $('-' * 80)
     Write-Host "Before tagging the new release"
     Write-Host "* Update .pot file." -foregroundcolor "White"
     Write-Host "* Update changelog." -foregroundcolor "White"
@@ -100,7 +139,13 @@ function checklist
 switch ($args[0])
 {
     "bump" {
+        header
         $newVersion = Read-Host 'New version number'
+        if ($newVersion -eq '') {
+            Write-Host "Exited..." -foregroundcolor "Red"
+            break
+        }
+        bump($newVersion)
         checklist
         break
     }
